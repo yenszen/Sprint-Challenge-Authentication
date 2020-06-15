a403c784f645594375932dc48dba518a6b2623c4
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-06-15 09:42:21 +0800

    completed authenticate middleware, register and login functionality

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,27 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (token) {
+      jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ message: "Not authenticated" });
+        } else {
+          req.decodedJwt = decodedToken;
+          console.log(req.decodedJwt);
+          next();
+        }
+      });
+    } else {
+      throw new Error("invalid auth data");
+    }
+  } catch (err) {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 };
